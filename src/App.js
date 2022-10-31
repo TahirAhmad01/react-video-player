9e2f186a43691aa9df46e0298d7a24044f19c45f
TahirAhmad01 <tahirahmad0234@gmail.com>
2022-10-31 16:31:39 +0600

    video player added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 function App() {
+  const [fileList, setList] = useState("")
+
+  const fileSelect = (e) => {
+    const uFiles = e.target.files[0];
+      console.log(e);
+      setList(URL.createObjectURL(uFiles))
+  }
+
+  console.log(fileList)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div><input type="file" id="upload" multiple onChange={fileSelect} accept="video/*"/> </div>
+
+      <div>{FileList}</div>
+
+      {fileList !== "" && <Video  loop muted
+            controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
+            // poster="http://sourceposter.jpg"
+            onCanPlayThrough={() => {
+                // Do stuff
+            }}>
+            <source src={fileList} type="video/webm" />
+            <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default />
+        </Video>}
     </div>
   );
 }
